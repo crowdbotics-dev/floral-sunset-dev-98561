759fd73bb954d7a0c91c22d90451fd7d0410cca1
Crowdbotics <engineering@crowdbotics.com>
2023-07-07 05:32:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "floral-sunset-dev-98561",
+  url: "https://floral-sunset-dev-98561.botics.co",
+  api: "https://floral-sunset-dev-98561.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
